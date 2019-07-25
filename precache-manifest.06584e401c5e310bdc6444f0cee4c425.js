aaa7e46ff7ac736003bcc505ee7c90c2f34c3ca0
Sevastian Sergiu <sevastian.sergiu@terranet.md>
2019-07-25 11:34:24 +0300

    Updates

---
+++ precache-manifest.06584e401c5e310bdc6444f0cee4c425.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ced4346436b45a0e9222a3f1ac59a3f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1ca59ad36bccf4818e96",
+    "url": "/monsters-rolodex/static/css/main.77798d5b.chunk.css"
+  },
+  {
+    "revision": "458a39bd49d85bb2560b",
+    "url": "/monsters-rolodex/static/js/2.b0d4d72d.chunk.js"
+  },
+  {
+    "revision": "1ca59ad36bccf4818e96",
+    "url": "/monsters-rolodex/static/js/main.93f1255e.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
